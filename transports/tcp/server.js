77e4b1373f0c348932a8b83c4c4395c014f17fa5
Pouya <pouya.eghbali@yandex.com>
2020-03-20 17:20:32 +0100

    Added TCP transport

---
+++ transports/tcp/server.js
@@ -0,0 +1,36 @@
+const readline = require("readline");
+const net = require("net");
+const { TCPSocket } = require("./socket");
+const { EventEmitter } = require("../../common");
+
+class Server extends EventEmitter {
+  constructor(config) {
+    super();
+    this.tcpConfig = config || Server.defaultTCPConfig();
+  }
+  static defaultTCPConfig() {
+    return { port: 4444, host: "0.0.0.0" };
+  }
+  createTCPServer() {
+    if (!this.tcpConfig) return;
+    const { port, host } = this.tcpConfig;
+    this.tcpServer = net.createServer();
+    this.tcpServer.listen(port, host);
+    this.tcpServer.on("connection", socket => this.onTCPConnect(socket));
+  }
+  onTCPConnect(socket) {
+    socket.rl = readline.createInterface(socket);
+    socket.rl.on("line", data => this.handleIncoming(socket, data));
+    socket.on("close", () => socket.rl.close());
+  }
+  handleIncoming(socket, data) {
+    const { instruction, details, id } = JSON.parse(data);
+    const tcpSocket = new TCPSocket(socket);
+    this.emit(instruction, tcpSocket, details, id);
+  }
+  start() {
+    return this.createTCPServer();
+  }
+}
+
+module.exports.Server = Server;
